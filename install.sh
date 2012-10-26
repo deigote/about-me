5c2e3d742ae82790f40febdf012bb2db22cfb1f8
deigote <diego@toharia.com>
2012-10-26 21:50:15 +0200

    Curl output is redirected to null

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ `git status -uno | grep "# Your branch is behind"` ]] ; then
 	git pull origin master
 	rm *.{css,js}_* *.ico.* 
 	groovy index.groovy
-        curl -X PURGE http://deigote.com/about-me
+        curl -X PURGE http://deigote.com/about-me &> /dev/null
 else 
 	echo "Everything is up to date"
 fi
